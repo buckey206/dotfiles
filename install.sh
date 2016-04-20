1f2241c97d932861402b517f03087202e134e733
jared <jared@Cloud>
2016-04-20 19:51:15 -0400

    updated install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
-sudo apt-get install curl vim tmux
+sudo apt-get install curl vim tmux zsh 
 
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 ln -s ~/dotfiles/i3 ~/.i3
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
