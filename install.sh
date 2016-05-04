ac0cff377a114698bde9fbb04d6684135bc87ddb
buckey <buckey206@gmail.com>
2016-05-04 01:41:40 -0700

    moved Changes to be committed: 	deleted:    1080pMonitor.sh 	deleted:    install.sh 	deleted:    packages.txt 	deleted:    regularScreen.sh

--- install.sh
+++
@@ -1,7 +0,0 @@
-#sudo apt-get install curl vim tmux zsh 
-
-ln -s ~/dotfiles/i3 ~/.i3
-#ln -s ~/dotfiles/vimrc ~/.vimrc
-#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-#ln -s ~/dotfiles/zshrc ~/.zshrc
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
