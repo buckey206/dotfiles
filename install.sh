14fc91ed5410bb503df5525baeea77faefc793bd
Jared <buckey206@gmail.com>
2016-09-12 20:46:40 -0700

    ls

--- install.sh
+++ install.sh
@@ -1,8 +1,8 @@
-#sudo apt-get install curl vim tmux zsh 
-##
-##ln -s ~/dotfiles/i3 ~/.i3
-##ln -s ~/dotfiles/vimrc ~/.vimrc
-##ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/bin ~/bin
+#sudo pacman -S curl vim tmux zsh 
+#
+#ln -s ~/dotfiles/i3 ~/.i3
+#ln -s ~/dotfiles/vimrc ~/.vimrc
+#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+#ln -s ~/dotfiles/bin ~/bin
 #sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+ln -s ~/dotfiles/zshrc ~/.zshrc
