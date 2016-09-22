93340b32d617c8c5c1431906248bca7a155decd4
Jared <buckey206@gmail.com>
2016-09-21 19:11:21 -0700

    tidied up the install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
-#ln -s ~/dotfiles/i3 ~/.i3
-#ln -s ~/dotfiles/vimrc ~/.vimrc
-#ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
-#ln -s ~/dotfiles/bin ~/bin
+git config --global user.email "buckey206@gmail.com"
+git config --global user.name "Jared"
+git config --global credential.helper cache
+git config --global credential.helper 'cache --timeout=3600'
+# This will set the account default identity and the password timeout to 1 hour instead of 15 minutes
 ln -s ~/dotfiles/i3 ~/.i3
 ln -s ~/dotfiles/vimrc ~/.vimrc
 ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/dotfiles/zshrc ~/.zshrc
 ln -s ~/dotfiles/bin ~/bin
-#sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-#ln -s ~/dotfiles/zshrc ~/.zshrc
+
